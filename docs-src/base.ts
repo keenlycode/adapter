92176921987c3557f9740746b617b8274ee86c69
Nitipit Nontasuwan <nitipit@gmail.com>
2022-11-18 21:32:52 +0700

    update

--- docs-src/base.ts
+++ docs-src/base.ts
@@ -9,6 +9,9 @@ import { injectGlobal as addStyle } from '@emotion/css';
 import { fontFluid } from 'gadjet/src/style/font-fluid';
 import { bgColor } from 'gadjet/src/style/bg-color';
 
+import { define, Adapter } from "../src/adapter";
+import { color } from './color';
+
 
 hljs.registerLanguage('js', javascript);
 hljs.registerLanguage('css', css);
@@ -60,3 +63,80 @@ code {
     justify-content: center;
 }
 `;
+
+
+class ID_Highlight extends Adapter {};
+define('id-highlight', ID_Highlight);
+ID_Highlight.tagStyle(`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    background-color: ${color.p3};
+    color: white;
+    .container {
+        width: 90%;
+        max-width: 600px;
+        margin: 2rem;
+        text-align: center;
+        border: 7px double white;
+        > h1 {
+            width: 100%;
+        }
+    }
+`)
+
+class Nav extends Adapter {};
+define('el-nav', Nav);
+Nav.tagStyle(`
+    display: flex;
+    width: 100%;
+    ${bgColor(color.p)}
+    cursor: pointer;
+    a {
+        padding: 0.5rem 1rem;
+        text-decoration: none;
+        font-weight: bold;
+        ${bgColor(color.p)}
+        &:hover {
+            ${bgColor('white')}
+        }
+    }
+`);
+
+class Paragraph extends Adapter {};
+define('el-paragraph', Paragraph);
+Paragraph.tagStyle(`
+    .container {
+        max-width: 45rem;
+        width: 90%;
+        display: flex;
+        justify-content: left;
+    }
+    h1, h2 {
+        & + p {
+            margin-top: 0;
+        }
+    }
+
+    p {
+        margin-top: 1rem;
+        width: 100%;
+        max-width: 45rem;
+    }
+    p + p {
+        margin-top: 0;
+    }
+    ol {
+        padding-left: 1.7rem;
+        margin-top: 0rem;
+    }
+`)
+
+
+class ID_Footer extends Adapter {};
+define('id-footer', ID_Footer);
+ID_Footer.tagStyle(`
+    display: flex;
+    min-height: 30vh;
+    width: 100%;
+`)
---
+++ docs-src/usage/usage.html.usage.md
@@ -0,0 +1,4 @@
+Adapter is a Web Component CSS library. It relies on <web-component/>
+and **CSS-in-JS** to acheive it's goals to be adaptive, extensible and modular as much as possible.
+
+gadjet establish it's core concept on StyledElement class which provides dynamic style methods to apply CSS-in-JS with <web-component/> as described in class structure below.
