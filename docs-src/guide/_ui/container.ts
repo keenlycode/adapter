33a1012b78472440c143a02236c76bf61a73ce24
Nitipit Nontasuwan <nitipit@gmail.com>
2024-01-06 20:17:18 +0700

    update docs

--- docs-src/guide/_ui/container.ts
+++ docs-src/guide/_ui/container.ts
@@ -2,17 +2,16 @@ import { Adapter } from '../../adapter';
 
 
 class Container extends Adapter {
-    static css = /*css*/`
-        & {
-            all: unset;
-            display: block;
-            box-sizing: border-box;
-            margin-bottom: 25dvh;
-            max-width: 80ch;
-            width: 90%;
-            transition: margin-left 0.3s ease-in-out;
-        }
-    `;
+    static css = /*css*/`& {
+        all: unset;
+        display: block;
+        box-sizing: border-box;
+        margin-bottom: 25dvh;
+        max-width: 80ch;
+        min-height: 100dvh;
+        width: 90%;
+        transition: margin-left 0.3s ease-in-out;
+    }`;
 
     constructor() {
         super();
@@ -30,12 +29,12 @@ class Container extends Adapter {
 
     defaultStyle(): string {
         const el_sidebar = document.querySelector('el-sidebar') as HTMLElement;
-        return /*css*/`
+        return /*css*/`& {
             @media screen and (min-width: 1200px) {
                 width: 68%;
                 margin-left: calc(${getComputedStyle(el_sidebar).width});
             }
-        `;
+        }`;
     }
 };
 
