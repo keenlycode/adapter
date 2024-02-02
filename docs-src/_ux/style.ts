b3571c0b62754187b456e79646c01a267c541cff
Nitipit Nontasuwan <nitipit@gmail.com>
2024-02-02 10:14:09 +0700

    add fontFluid

--- docs-src/_ux/style.ts
+++ docs-src/_ux/style.ts
@@ -1,7 +1,9 @@
 import Color from "color";
 
+const css = String.raw;
+
 function bgColor(color) {
-    return /*css*/`
+    return css`
         background-color: ${color};
         color: ${Color(color).isDark() ? 'white' : 'black'};
     `.trim();
@@ -19,7 +21,7 @@ function pxToRem(px: any) {
 }
 
 const aspectRatio = (ratio: string = '1/1'): string => {
-    return `
+    return css`
     height: auto;
     aspect-ratio: ${ratio};
     @supports not (aspect-ratio: ${ratio}) {
@@ -38,4 +40,36 @@ const aspectRatio = (ratio: string = '1/1'): string => {
     `.trim();
 }
 
-export { bgColor, lift, pxToRem, aspectRatio };
+interface FontFluidParam {
+    vwMin?: number,
+    vwMax?: number,
+    fontSizeMin?: number,
+    fontSizeMax?: number
+}
+
+function fontFluid({
+    vwMin = 300, vwMax = 1200,
+    fontSizeMin = 16, fontSizeMax = 18
+}: FontFluidParam = {}): string {
+    let viewportRatio = `(100vw - ${vwMin}px) / (${vwMax} - ${vwMin})`;
+    let fontScaleRatio = `(${fontSizeMax} - ${fontSizeMin}) * ${viewportRatio}`;
+    return css`
+        font-size: ${fontSizeMin}px;
+        @media screen and (min-width: ${vwMin}px) {
+            font-size: calc(
+                ${fontSizeMin}px + ${fontScaleRatio}
+            );
+        }
+        @media screen and (min-width: ${vwMax}px) {
+            font-size: ${fontSizeMax}px;
+        }
+    `.trim();
+}
+
+export {
+    bgColor,
+    lift,
+    pxToRem,
+    aspectRatio,
+    fontFluid
+};
