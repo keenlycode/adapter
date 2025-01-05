0cc87a10edb31b3e26aadaf13b873c8660f53902
Nitipit Nontasuwan <nitipitn@unops.org>
2025-01-05 23:10:56 +0700

    update

--- esbuild/dist.js
+++ esbuild/dist.js
@@ -5,41 +5,12 @@ import { glob } from "glob";
 import { platform } from "node:process";
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 let files, result = null;
 
-// let files = await glob(path.join(__dirname, "../src/**/*.{ts,js}"), {
-//   ignore: path.join(__dirname, "../src/**/*.bundle.{ts,js}"),
-// });
-
-// console.log(
-// `📦️ Create adapter distributed files
-// ===================================`);
-
-// let result = await esbuild.build({
-//   entryPoints: files,
-//   bundle: false,
-//   outdir: "dist/browser/",
-//   outbase: path.join(__dirname, "../src/"),
-//   format: "esm",
-//   platform: "browser",
-//   lineLimit: 80,
-//   keepNames: false,
-//   minify: false,
-//   sourcemap: true,
-//   metafile: true,
-//   logLevel: "info",
-// });
-
-// console.log(
-//   await esbuild.analyzeMetafile(result.metafile, {
-//     verbose: true,
-//   })
-// );
-
-files = await glob(path.join(__dirname, "../src/**/*.bundle.{ts,js}"));
+files = await glob("src/**/*.bundle.{ts,js}");
 
 console.log(
 `📦️ Create library bundle files
@@ -49,12 +20,42 @@ result = await esbuild.build({
   entryPoints: files,
   bundle: true,
   outdir: "dist/browser/",
-  outbase: path.join(__dirname, "../src/"),
+  outbase: "src/",
   format: "esm",
   platform: "browser",
   lineLimit: 80,
   keepNames: false,
   minify: true,
+  sourcemap: true,
+  metafile: true,
+  logLevel: "info",
+});
+
+console.log(
+  await esbuild.analyzeMetafile(result.metafile, {
+    verbose: true,
+  })
+);
+
+files = await glob("dist/browser/**/*.js", {
+  ignore: "dist/**/*.bundle.js"
+});
+
+console.log(
+`📦️ Create adapter distributed files
+===================================`);
+
+result = await esbuild.build({
+  entryPoints: files,
+  bundle: false,
+  outdir: "dist/browser/",
+  outbase: "dist/browser/",
+  format: "esm",
+  platform: "browser",
+  lineLimit: 80,
+  keepNames: false,
+  minify: true,
+  allowOverwrite: true,
   sourcemap: true,
   metafile: true,
   logLevel: "info",
--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,3 @@
 export { Adapter, AdapterMixin } from './adapter.ts';
-export { postcss } from './css/postcss.ts';
 export { stylis } from './css/stylis.ts';
+export { postcss } from './css/postcss.ts';
